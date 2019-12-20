e09a5599ebc5b8b1f4bfbf5d6618eb7bf85e4c7c
IbragimKariev <ibrosh_96@mail.ru>
2019-12-20 11:33:39 +0600

    commit-5

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ $(document ).ready(function() {
 $('.sub-menu ul').hide();
 $(".sub-menu a").click(function () {
     $(this).parent(".sub-menu").children("ul").slideToggle("100");
-    $(this).find(".right").toggleClass(" icon-up-open");
+    $(this).find("icon-ctrl").toggleClass(".rotate");
 });
 
 
